f469940feb10b45a6b8d8e06fafb9bc365c132d9
Jason Chin <cschin@echo-air.gateway.2wire.net>
2011-04-10 13:50:30 -0700

    add license statement

--- src/PypeTest.py
+++ src/PypeTest.py
@@ -1,3 +1,27 @@
+
+# @author Jason Chin
+#
+# Copyright (C) 2010 by Jason Chin 
+# Copyright (C) 2011 by Jason Chin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import sys
 import os 
 
@@ -58,9 +82,9 @@ def simpleTest():
     os.system("sleep 1;touch %s;" % f3.localFileName)
 
 def testDistributed(runmode, cleanup):
-    baseDir = "/home/UNIXHOME/jchin/task2011/PypeEngineIntegrationTest/src"
+    #baseDir = "/home/UNIXHOME/jchin/task2011/PypeEngineIntegrationTest/src"
     baseDir = "/Users/cschin/Sandbox/PypeEngine/src"
-    baseDir = "/home/cschin/Sandbox/PypeEngine/src"
+    #baseDir = "/home/cschin/Sandbox/PypeEngine/src"
     import random
     random.seed(1984)
     #mq = Queue()
