67f3f05c6c7a08bd164531e30c66eccd38581d42
Jason Chin <cschin@echo-air.gateway.2wire.net>
2011-04-10 16:54:15 -0700

    fix typos

--- src/PypeCommon.py
+++ src/PypeCommon.py
@@ -118,7 +118,7 @@ class PypeObject(object):
 def runShellCmd(args):
 
     """ 
-    Utility funtion taht run a shell script command. 
+    Utility funtion that run a shell script command. 
     I blocks until the command is finished. The return value
     from the shell command is returned
     """
@@ -135,7 +135,7 @@ def runShellCmd(args):
 def runSgeSyncJob(args):
 
     """ 
-    Utility funtion taht run a shell script with SGE. 
+    Utility funtion that run a shell script with SGE. 
     I blocks until the command is finished. The return value
     from the shell command is returned
     """
