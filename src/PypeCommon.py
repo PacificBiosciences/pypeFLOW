3f123e6f3f38864f20a03f9b10f3d28589d3475a
Jason Chin <jchin@pacificbiosciences.com>
2011-04-11 20:45:47 -0700

    add PacBio as copyright owner as some code is contributed by PacBio employee

--- src/PypeCommon.py
+++ src/PypeCommon.py
@@ -2,7 +2,7 @@
 # @author Jason Chin
 #
 # Copyright (C) 2010 by Jason Chin 
-# Copyright (C) 2011 by Jason Chin
+# Copyright (C) 2011 by Jason Chin, Pacific Biosciences
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
